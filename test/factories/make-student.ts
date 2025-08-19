2fd43e78ae6530b940ab43b6a0a07dec3e4ca682
ALOISIO SAMPAIO <aloisiocairo@gmail.com>
2025-08-19 07:07:21 -0300

    refactor: Utilizando factories nos testes E2E

--- test/factories/make-student.ts
+++ test/factories/make-student.ts
@@ -7,6 +7,9 @@ import {
   Student,
   StudentProps,
 } from '@/domain/forum/enterprise/entities/student'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaStudentMapper } from '@/infra/database/prisma/mappers/prisma-student-mapper'
 
 export function makeStudent(
   override: Partial<StudentProps> = {},
@@ -23,4 +26,20 @@ export function makeStudent(
   )
 
   return student
+}
+
+// Node.js > Nest.js > Implementando controllers e testes > Utilizando Factories nos Testes E2E
+@Injectable()
+export class StudentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaStudent(data: Partial<StudentProps> = {}): Promise<Student> {
+    const student = makeStudent(data)
+
+    await this.prisma.user.create({
+      data: PrismaStudentMapper.toPrisma(student),
+    })
+
+    return student
+  }
 }
