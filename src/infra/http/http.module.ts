b57cecb88c22fe48f925078cecc94ad16cde960d
ALOISIO SAMPAIO <aloisiocairo@gmail.com>
2025-08-19 15:15:09 -0300

    feat: Controller de editar pergunta

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -12,6 +12,8 @@ import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/reg
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student';
 import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller';
 import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug';
+import { EditQuestionController } from './controllers/edit-question.controller';
+import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -20,14 +22,16 @@ import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/g
     AuthenticateController, 
     CreateQuestionController, 
     FetchRecentQuestionsController,
-    GetQuestionBySlugController
+    GetQuestionBySlugController,
+    EditQuestionController,
   ],
   providers: [
     CreateQuestionUseCase,
     FetchRecentQuestionsUseCase,
     RegisterStudentUseCase,
     AuthenticateStudentUseCase,
-    GetQuestionBySlugUseCase
+    GetQuestionBySlugUseCase,
+    EditQuestionUseCase,
   ],
 })
 export class HttpModule {}
